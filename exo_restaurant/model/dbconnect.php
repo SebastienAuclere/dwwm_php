4143bf95e1bf4dd218568ed62a5b5a5fa927c3f1
SebastienAuclere <137295360+SebastienAuclere@users.noreply.github.com>
2024-10-15 15:56:08 +0200

    commit fin de commentaires

--- exo_restaurant/model/dbconnect.php
+++ exo_restaurant/model/dbconnect.php
@@ -4,25 +4,34 @@
 
         //attributs
 
-        private static ?PDO $connection = null;  //
-        private const HOST = "localhost";        //
-        private const DBNAME = "guide";          //
-        private const USER = "root";             //
-        private const PWD = "";                  //
+        private static ?PDO $connection = null;  // attr. prive static type pdo initialise null represente la connexion a la base de donnee
+        private const HOST = "localhost";        // constante privee stocke l'hote de la base de donnee ici "localhost"
+        private const DBNAME = "guide";          // constante privée qui stocke le nom de la base de données, ici “guide"
+        private const USER = "root";             // constante privée qui stocke le nom d’utilisateur pour la connexion à la base de données, ici “root”
+        private const PWD = "";                  // constante privée qui stocke le mot de passe pour la connexion à la base de données, ici une chaîne vide
 
         //constructeur
 
-        private function __construct() {}        //
-
+        private function __construct() {}        // __construct : Un constructeur privé pour empêcher l’instanciation directe de la classe. 
+                                                 // Cela garantit que la classe ne peut être instanciée qu’à travers la méthode getInstance. 
         //fonctions
 
-        // getInstance permet de
+        // getInstance : Une méthode publique et statique qui retourne une instance de PDO
 
-        public static function getInstance(): PDO  //
+        public static function getInstance(): PDO  
         {
-            if (is_null(self::$connection)) {      //
-                self::$connection = new PDO("mysql:host=" . self::HOST . "; dbname=" . self::DBNAME . ";charset=utf8mb4", self::USER, self::PWD);  //
-            }
-            return self::$connection;              //
+            if (is_null(self::$connection)) {      // Si l’attribut $connection est null, 
+                self::$connection = new PDO("mysql:host="    // il crée une nouvelle instance de PDO 
+                . self::HOST . "; dbname=" . self::DBNAME .  // avec les paramètres de connexion définis
+                ";charset=utf8mb4", self::USER, self::PWD);  // par les constantes HOST, DBNAME, USER, et PWD
+            }                                                // La connexion utilise le charset utf8mb4 pour assurer 
+                                                             // une bonne gestion des caractères spéciaux.
+            return self::$connection;      // La connexion utilise le charset utf8mb4 pour assurer une bonne gestion
+                                           // des caractères spéciaux.                                          
         }
     }
+
+
+    // resume : Cette classe utilise le pattern Singleton pour s’assurer qu’il n’y a qu’une seule connexion
+    // à la base de données à tout moment. 
+    // Cela permet d’optimiser les ressources et de gérer efficacement la connexion à la base de données.
--- exo_restaurant/model/liste_resto.php
+++ exo_restaurant/model/liste_resto.php
@@ -4,66 +4,66 @@ class RestoRepository
 
     //attributs
 
-    private PDO $connect;        //
-    private string $nomTable;    //
-    private array $tabChamp;     //
+    private PDO $connect;        // Un attribut privé de type PDO qui représente la connexion à la base de données.
+    private string $nomTable;    // Un attribut privé de type string qui stocke le nom de la table dans la base de données.
+    private array $tabChamp;     // Un attribut privé de type array qui stocke les champs de la table.
 
     //constructeur
 
-    public function __construct(string $_nomTable, array $_tabChamp = []) {  //
-       
-        $this->connect = Dbconnect::getInstance();  //
-        $this->nomTable = $_nomTable;               //
-        $this->tabChamp = $_tabChamp;               //
+    public function __construct(string $_nomTable, array $_tabChamp = []) {  // Un constructeur public qui initialise 
+                                                                             // les attributs de la classe
+        $this->connect = Dbconnect::getInstance();  //  La connexion à la base de données est établie en utilisant la méthode getInstance de la classe Dbconnect
+        $this->nomTable = $_nomTable;               //  Le nom de la table est passé en paramètre et assigné à l’attribut $nomTable
+        $this->tabChamp = $_tabChamp;               //  Un tableau de champs est passé en paramètre et assigné à l’attribut $tabChamp. Ce paramètre est optionnel et par défaut, c’est un tableau vide.
     }
 
     //fonctions
 
-    public function chercherResteau($_id): array /// chercher le restaurant par l'id 
-    {
-        $rq = "select * from " . $this->nomTable . " where soundex(id) = soundex(:idresto)";  //
-        $res = $this->connect->prepare($rq);                                                  //
-        $res->bindParam(":idresto", $_id, PDO::PARAM_STR);                                    // 
-        $res->execute();                                                                      //
-        if (($row = $res->fetch(PDO::FETCH_ASSOC)) == false) {                                //
-            $row = [];                                                                        //
-            return $row;                                                                      //
-        } else {                                                                              //
-            return $row;                                                                      //
+    public function chercherResteau($_id): array /// Cette fonction prend un identifiant ($_id) en paramètre et retourne un tableau contenant les informations d’un restaurant correspondant à cet identifiant.
+    {                                             // La fonction est publique et retourne un tableau.
+        $rq = "select * from " . $this->nomTable . " where soundex(id) = soundex(:idresto)";  // La requête SQL sélectionne toutes les colonnes de la table spécifiée par $this->nomTable où le soundex de l’identifiant de la table est égal au soundex de l’identifiant fourni (:idresto). Soundex est une fonction qui permet de comparer des chaînes de caractères en fonction de leur prononciation.
+        $res = $this->connect->prepare($rq);                                                  // La requête SQL est préparée pour être exécutée.
+        $res->bindParam(":idresto", $_id, PDO::PARAM_STR);                                    // Le paramètre :idresto est lié à la valeur de $_id en tant que chaîne de caractères.
+        $res->execute();                                                                      // La requête est exécutée.
+        if (($row = $res->fetch(PDO::FETCH_ASSOC)) == false) {                                // La fonction tente de récupérer une ligne de résultat sous forme de tableau associatif.
+            $row = [];                                                                        // Si aucune ligne n’est trouvée (fetch retourne false), un tableau vide est retourné.
+            return $row;                                                                      // Sinon, le tableau contenant les données de la ligne est retourné.
+        } else {                                                                              // En résumé, cette fonction cherche un restaurant par son identifiant en utilisant une comparaison phonétique et retourne les informations du restaurant sous forme de tableau associatif, ou un tableau vide si aucun restaurant n’est trouvé.
+            return $row;                                                                      //  ou un tableau vide si aucun restaurant n’est trouvé.
         }
     }
 
-    public function searchOne($_nomResteau): array /// chercher le restaurant par le nom du resteau 
-    {
-        $rq = "select * from " . $this->nomTable . " where soundex(nom) = soundex(:nomresto)";  //
-        $res = $this->connect->prepare($rq);                                                    //
-        $res->bindParam(":nomresto", $_nomResteau, PDO::PARAM_STR);                             //  
-        $res->execute();                                                                        //
-        if (($row = $res->fetch(PDO::FETCH_ASSOC)) == false) {                                  //
-            $row = [];                                                                          //
-            return $row;                                                                        //
-        } else {                                                                                // 
-            return $row;                                                                        //
+    public function searchOne($_nomResteau): array /// Cette fonction prend un nom de restaurant ($_nomResteau) en paramètre et retourne un tableau contenant les informations d’un restaurant correspondant à ce nom 
+    {                                               // La fonction est publique et retourne un tableau.
+        $rq = "select * from " . $this->nomTable . " where soundex(nom) = soundex(:nomresto)";  // La requête SQL sélectionne toutes les colonnes de la table spécifiée par $this->nomTable où le soundex du nom de la table est égal au soundex du nom fourni (:nomresto). Soundex est une fonction qui permet de comparer des chaînes de caractères en fonction de leur prononciation.
+        $res = $this->connect->prepare($rq);                                                    // La requête SQL est préparée pour être exécutée.
+        $res->bindParam(":nomresto", $_nomResteau, PDO::PARAM_STR);                             // Le paramètre :nomresto est lié à la valeur de $_nomResteau en tant que chaîne de caractères. 
+        $res->execute();                                                                        // La requête est exécutée.
+        if (($row = $res->fetch(PDO::FETCH_ASSOC)) == false) {                                  // La fonction tente de récupérer une ligne de résultat sous forme de tableau associatif.
+            $row = [];                                                                          // Si aucune ligne n’est trouvée (fetch retourne false), un tableau vide est retourné.
+            return $row;                                                                        // Sinon, le tableau contenant les données de la ligne est retourné.
+        } else {                                                                                // En résumé, cette fonction cherche un restaurant par son nom en utilisant une comparaison phonétique et retourne les informations du restaurant sous forme de tableau associatif, ou un tableau vide si aucun restaurant n’est trouvé.
+            return $row;                                                                        // et retourne les informations du restaurant sous forme de tableau associatif, ou un tableau vide si aucun restaurant n’est trouvé.
         }
     }
 
-    public function searchAll(): array /// donne tout le tableau
+    public function searchAll(): array // Cette fonction ne prend aucun paramètre et retourne un tableau contenant toutes les lignes de la table spécifiée
     {
-        $rq = "select * from " . $this->nomTable;                                               //
-        $res = $this->connect->query($rq, PDO::FETCH_ASSOC);                                    //  
-        $tabData = $res->fetchAll();                                                            //
-        return $tabData;                                                                        // 
-    }    
+        $rq = "select * from " . $this->nomTable;                                               // La requête SQL sélectionne toutes les colonnes de toutes les lignes de la table spécifiée par $this->nomTable.
+        $res = $this->connect->query($rq, PDO::FETCH_ASSOC);                                    // La requête est exécutée et les résultats sont récupérés sous forme de tableau associatif (PDO::FETCH_ASSOC). 
+        $tabData = $res->fetchAll();                                                            // Toutes les lignes de résultats sont récupérées et stockées dans le tableau $tabData.
+        return $tabData;                                                                        // Le tableau contenant toutes les lignes de la table est retourné.
+    }                                                                                           // En résumé, cette fonction récupère toutes les lignes de la table spécifiée et retourne les résultats sous forme de tableau associatif.
 
-    public function ajouterResteau(string $_nom, string $_adresse, float $_prix, string $_Commentaire, int $_Note, string $_visite): bool  /// ajoute un resteau
-    {
-        $rq = "insert into " . $this->nomTable . " values (id, '" . $_nom . "','" . $_adresse . "',"  //
-              . $_prix . ",'" . $_Commentaire . "'," . $_Note . ",'" . $_visite . "')";               //
-        echo $rq;                                                                                     //
-        $numberLigne = $this->connect->exec($rq);                                                     //
-        if ($numberLigne == 1) {                                                                      //
+    public function ajouterResteau(string $_nom, string $_adresse, float $_prix, string $_Commentaire, int $_Note, string $_visite): bool  // Cette fonction prend plusieurs paramètres relatifs à un restaurant et insère une nouvelle ligne dans la table correspondante
+    {                                                                                                                                      //La fonction est publique et retourne un booléen (true ou false)  
+        $rq = "insert into " . $this->nomTable . " values (id, '" . $_nom . "','" . $_adresse . "',"  // La requête SQL insère une nouvelle ligne dans la table spécifiée par $this->nomTable. Les valeurs des colonnes sont fournies par les paramètres de la fonction. 
+              . $_prix . ",'" . $_Commentaire . "'," . $_Note . ",'" . $_visite . "')";               // Notez que id est supposé être une colonne auto-incrémentée ou gérée par la base de données.
+        echo $rq;                                                                                     // La requête SQL est affichée, probablement à des fins de débogage.
+        $numberLigne = $this->connect->exec($rq);                                                     // La requête est exécutée et le nombre de lignes affectées est stocké dans $numberLigne.
+        if ($numberLigne == 1) {                                                                      // Si une ligne a été insérée ($numberLigne == 1), la fonction retourne true.
             return true;                                                                              // 
         }                                                                                             //
-        return false;                                                                                 //
-    }
+        return false;                                                                                 // Sinon, elle retourne false.
+    }                                                                                                 // En résumé, cette fonction ajoute un nouveau restaurant dans la table et retourne true si l’insertion a réussi, ou false sinon.
 }
