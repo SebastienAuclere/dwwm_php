a6fbd7f2b1ab222570ee0f2a76fded6bd8196fc2
SebastienAuclere <137295360+SebastienAuclere@users.noreply.github.com>
2024-10-16 11:12:05 +0200

    formulaire

--- exo_restaurant/ajouter.php
+++ exo_restaurant/ajouter.php
@@ -1,3 +1,22 @@
+<?php
+
+require "./model/liste_resto.php";
+require "./model/dbconnect.php";
+$myObject = new RestoRepository("restaurants");
+
+if (isset($_POST["send"])) {
+    $test = $myObject->ajouterResteau($_POST["name"], $_POST["address"], floatval($_POST["price"]), $_POST["comments"], intval($_POST["note"]), $_POST["visit"]);
+    if ($test === true) {
+        // retour a la page d'accueil
+        header("Location:./index.php");  // "header" est une fonction qui redirige vers une page
+    } else {
+        echo "ajout du restaurant a echoue veuillez recommencer";
+    }
+}
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="fr-fr">
 
@@ -6,13 +25,13 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>resto</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='styleform.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='./mon_css/styleform.css'>
     <script src='main.js'></script>
 </head>
 
 <body>
     <div>
-        <form action="" method="get" class="background">
+        <form action="" method="POST" class="background">
             <div>
                 <label for="name">name:</label>
                 <input type="text" id="name" name="name" class="center1">
@@ -20,7 +39,7 @@
             <div>
                 <label for="address">address:</label>
                 <input type="text" id="address" name="address" class="center2">
-            </div>   
+            </div>
             <div>
                 <label for="price">price (between 0 and 200):</label>
                 <input type="number" id="price" name="price" min="0" max="200" class="center3">
@@ -33,11 +52,11 @@
                 <input type="number" id="note" name="note" min="0" max="10" class="center5">
             </div>
             <div>
-                <label for="visit">Enter a date before 1980-01-01:</label>
-                <input type="date" id="visit" name="visit" max="1979-12-31" class="center6">
+                <label for="visit">Enter a date before 2024-01-01:</label>
+                <input type="date" id="visit" name="visit" max="2024-12-10" class="center6">
             </div>
             <div>
-                <input type="submit" value="Submit" class="center7">
+                <input type="submit" value="send" name="send" class="center7">
             </div>
         </form>
     </div>
--- exo_restaurant/index.php
+++ exo_restaurant/index.php
@@ -10,12 +10,12 @@
 
 <body>
     <?php                                                                                  // inclusion des fichiers requis : 
-        require "./model/dbconnect.php";                                                   // Ces lignes incluent les fichiers dbconnect.php et liste_resto.php. 
-        require "./model/liste_resto.php";                                                 // Le mot-clé require est utilisé pour inclure et évaluer les fichiers spécifiés.
-                                                                                           // Si l’un des fichiers n’est pas trouvé, une erreur fatale sera générée et l’exécution du script s’arrêtera.
-        $mylist = new RestoRepository("restaurants");                                      // Création d’une instance de RestoRepository 
-    ?>                                                                                     
-    <form action="" method="get" enctype="text/plain">                                     
+    require "./model/dbconnect.php";                                                   // Ces lignes incluent les fichiers dbconnect.php et liste_resto.php. 
+    require "./model/liste_resto.php";                                                 // Le mot-clé require est utilisé pour inclure et évaluer les fichiers spécifiés.
+    // Si l’un des fichiers n’est pas trouvé, une erreur fatale sera générée et l’exécution du script s’arrêtera.
+    $mylist = new RestoRepository("restaurants");                                      // Création d’une instance de RestoRepository 
+    ?>
+    <!-- <form action="" method="get" enctype="text/plain">                                     
             <div>
                 <label for="username" class="champ">Nom d'utilisateur:</label>
                 <input type="text" id="username" name="username" class="champ"></label>
@@ -23,34 +23,40 @@
             <div class="champ">
                 <label for="password">Mot de Passe:</label>
                 <input type="text" id="password" max="50" name="password" 
-                       value="<?php echo "khgkgkgjk"; ?>" class="champ2">         <!-- entree d'une valeur -->
+                       value="<" class="champ2">         entree d'une valeur 
                 <input type="submit" value="Valider" name="validation" class="butvalid">
-            </div>
-            <p>Tableau des restaurants:</p>
-            <div class="tailletab">
-                <?php
+            </div></form>-->
 
-                $data = $mylist->searchAll();                                                  // Récupération des données : Cette ligne appelle la méthode searchAll de l’objet $mylist (instance de RestoRepository) 
-                                                                                               // pour récupérer toutes les lignes de la table restaurants. Les données sont stockées dans le tableau $data. 
-                echo "<table><thead><tr><th>id</th><th>nom</th><th>adresse</th><th>prix</th>   
+
+    <p>Tableau des restaurants:</p>
+
+
+    <a href="ajouter.php" target="_blank"> Ajouter</a><br><br><br>
+    <div class="tailletab">
+        <?php
+
+        $data = $mylist->searchAll();                                                  // Récupération des données : Cette ligne appelle la méthode searchAll de l’objet $mylist (instance de RestoRepository) 
+        // pour récupérer toutes les lignes de la table restaurants. Les données sont stockées dans le tableau $data. 
+        echo "<table><thead><tr><th>id</th><th>nom</th><th>adresse</th><th>prix</th>   
                 <th>commentaire</th><th>note</th><th>visite></th></tr></thead><tbody>";        // Affichage du tableau HTML : Cette ligne commence la construction d’un tableau HTML avec une ligne d’en-tête contenant les noms des colonnes.
 
-                for ($i = 0; $i < count($data); $i++) {                                        // Boucle pour afficher les données : La boucle for parcourt chaque élément du tableau $data
-                    echo "<tr>";                                                               // Pour chaque ligne de données, une nouvelle ligne de tableau (<tr>) est créée.
-                    foreach ($data[$i] as $key => $value) {                                    // La boucle foreach parcourt chaque colonne de la ligne actuelle et affiche la valeur dans une cellule de tableau (<td>).
-                        echo "<td>" . $value . "</td>";                                        // 
-                    }                                                                          //
-                    echo "</tr>";                                                              // 
-                }                                                                              // Fermeture du tableau HTML:
-                echo "</tbody></table>";                                                       // Cette ligne ferme le corps (<tbody>) et le tableau (<table>).
+        for ($i = 0; $i < count($data); $i++) {                                        // Boucle pour afficher les données : La boucle for parcourt chaque élément du tableau $data
+            echo "<tr>";                                                               // Pour chaque ligne de données, une nouvelle ligne de tableau (<tr>) est créée.
+            foreach ($data[$i] as $key => $value) {                                    // La boucle foreach parcourt chaque colonne de la ligne actuelle et affiche la valeur dans une cellule de tableau (<td>).
+                echo "<td>" . $value . "</td>";                                        // 
+            }                                                                          //
+            echo "</tr>";                                                              // 
+        }                                                                              // Fermeture du tableau HTML:
+        echo "</tbody></table>";                                                       // Cette ligne ferme le corps (<tbody>) et le tableau (<table>).
 
-                echo $mylist->ajouterResteau("Burger King", "rue des Mimosas", 15, "C\'est pour les enfants", 5, "2024-05-06");  // Ajout d’un nouveau restaurant : Cette ligne appelle la méthode ajouterResteau de l’objet $mylist pour ajouter un nouveau restaurant avec les informations fournies 
-                                                                                                //La méthode ajouterResteau retourne true si l’insertion a réussi, sinon false. Le résultat est affiché.
-                
-                        // En résumé, ce code récupère toutes les données des restaurants, les affiche dans un tableau HTML, puis ajoute un nouveau restaurant et affiche le résultat de cette opération.
-                ?>
-            </div>
-        </div>
-    </form>
+        echo $mylist->ajouterResteau("Burger King", "rue des Mimosas", 15, "C\'est pour les enfants", 5, "2024-05-06");  // Ajout d’un nouveau restaurant : Cette ligne appelle la méthode ajouterResteau de l’objet $mylist pour ajouter un nouveau restaurant avec les informations fournies 
+        //La méthode ajouterResteau retourne true si l’insertion a réussi, sinon false. Le résultat est affiché.
+
+        // En résumé, ce code récupère toutes les données des restaurants, les affiche dans un tableau HTML, puis ajoute un nouveau restaurant et affiche le résultat de cette opération.
+        ?>
+    </div>
+    </div>
+
 </body>
+
 </html>
