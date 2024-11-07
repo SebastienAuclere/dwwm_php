488145fd7a7a0e35b92e05401775dc6156f29d72
balistik23 <auclere.seb@gmail.com>
2024-11-07 16:08:27 +0100

    formulaire css a terminer

--- exo_voilier/index.php
+++ exo_voilier/index.php
@@ -13,16 +13,18 @@
 
 <body>
     <a href="liste.php">cliquer ici</a>
-    <hr>
-    <form action="" method="get">
-        <h1>ACCES MEMBRES</h1>
-        <label for="email">email:</label>
-        <input type="email" id="email" name="email">
-        <label for="pwd">Mot de passe</label><br>
-        <input type="password" id="pwd" name="pwd" class="bagrcolor">
-        <input type="button" value="Valider" class="stylbut">
-    </form>
-    <hr>
+    <div>
+        <hr>
+        <form action="" method="get">
+            <h2>ACCES MEMBRES</h2>
+            <label for="email" class="bottom">email</label>
+            <input type="email" id="email" name="email">
+            <label for="pwd" class="top">Mot de passe</label><br>
+            <input type="password" id="pwd" name="pwd" class="bagrcolor">
+            <input type="button" value="Valider" class="stylbut">
+        </form>
+        <hr>
+    </div>
 </body>
 
 </html>
