ff804b6079997e4e22906e57c4be9bf101c4b9d1
balistik23 <auclere.seb@gmail.com>
2024-11-08 11:30:33 +0100

    exo voilier db connect

--- exo_voilier/php/index.php
+++ exo_voilier/php/index.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>index.php</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
@@ -18,9 +18,9 @@
         <form action="" method="get">
             <h2>ACCES MEMBRES</h2>
             <label for="email" class="bottom">email</label>
-            <input type="email" id="email" name="email">
+            <input type="email" id="email" name="email" class="inputstyle1">
             <label for="pwd" class="top">Mot de passe</label><br>
-            <input type="password" id="pwd" name="pwd" class="bagrcolor">
+            <input type="password" id="pwd" name="pwd" class="inputstyle2">
             <input type="button" value="Valider" class="stylbut">
         </form>
         <hr>
