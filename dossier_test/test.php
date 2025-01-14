8453af4f70501400d3cfdecf6f4c55f50c440573
balistik23 <auclere.seb@gmail.com>
2025-01-14 08:50:10 +0100

    mise a jour

--- dossier_test/test.php
+++ dossier_test/test.php
@@ -1,43 +1,48 @@
 <?php
 
-class User 
-{
-    private string $name = "Seb";
+// class User 
+// {
+//     private string $name = "Seb";
 
-    public function __toString() {
-        return $this->name;
-    }
-}
+//     public function __toString() {
+//         return $this->name;
+//     }
+// }
 
-$booleen = true;
-$nombre = 42;
-$reel = 13.37;
-$chaine = "Mike";
-$tableau = ["Joe", "Jack", "Cindy" ];
-$objet = new User();
+// $booleen = true;
+// $nombre = 42;
+// $reel = 13.37;
+// $chaine = "Mike";
+// $tableau = ["Joe", "Jack", "Cindy" ];
+// $objet = new User();
 
-echo $booleen ."<br/>";
-echo $nombre ."<br/>";
-echo $reel ."<br/>";
-echo $chaine ."<br/>";
-echo $tableau ."<br/>";
-echo $objet;
+// echo $booleen ."<br/>";
+// echo $nombre ."<br/>";
+// echo $reel ."<br/>";
+// echo $chaine ."<br/>";
+// echo "<pre>";
+// echo $tableau;
+// echo "</pre>";
+// echo $objet ."<br/>";
 
-//phpinfo();
+// //phpinfo();
 
-echo "<pre>";
-var_dump($tableau);
-echo "</pre>";
-echo "<pre>";
-var_dump($objet)."<br/>";
-echo "</pre>";
+// echo "<pre>";
+// var_dump($tableau);
+// echo "</pre>";
+// echo "<pre>";
+// var_dump($objet)."<br/>";
+// echo "</pre>";
 
 
-echo "<pre>";
-var_export($tableau)."<br/>";
-echo "</pre>";
-echo "<pre>";
-var_export($objet)."<br/>";
-echo "</pre>";
+// echo "<pre>";
+// var_export($tableau)."<br/>";
+// echo "</pre>";
+// echo "<pre>";
+// var_export($objet)."<br/>";
+// echo "</pre>";
+
+echo '<h1>Hello World!</h1>
+<p>This is my first PHP page.</p>';
 
 ?> 
