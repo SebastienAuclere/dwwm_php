cc993f5c997530ca82c0afacf6fd416a1e32be2f
balistik23 <auclere.seb@gmail.com>
2025-01-27 15:27:56 +0100

    maj

---
+++ test_openclassroom/affichertexte.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ma page web</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h1>Ma page web</h1>        
+        <p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+    </body>
+</html>
