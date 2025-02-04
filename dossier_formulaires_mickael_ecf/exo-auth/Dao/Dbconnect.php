5be2b8d64d9eb58173d153956a2e90c48340c97a
balistik23 <auclere.seb@gmail.com>
2025-02-04 16:20:32 +0100

    connexion

--- dossier_formulaires_mickael_ecf/exo-auth/Dao/Dbconnect.php
+++ dossier_formulaires_mickael_ecf/exo-auth/Dao/Dbconnect.php
@@ -14,7 +14,7 @@ class DbConnect
     */
     public static function getInstance(): PDO {
         if(self::$instance === null) {
-            self::$instance = new PDO('mysql:host=localhost;port=3306;dbname=demo_auth;charset=utf8', 'root', '');
+            self::$instance = new PDO('mysql:host=mysql-ergatesfaber.alwaysdata.net;port=3306;dbname=ergatesfaber_users;charset=utf8', '397666', 'brigitte2145*');
         }
 
         return self::$instance;
